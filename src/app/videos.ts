ec07b80f9324a4da23291fa70cef7807fc71309a
christine <christamsnow@gmail.com>
2020-12-26 22:07:37 -0500

    css changes to image sizing

--- src/app/videos.ts
+++ src/app/videos.ts
@@ -1,32 +1,38 @@
 export const videos = [
   {
+    title: 'World`s Worst Makeup Tutorial',
+    postedBy: 'christine',
+    description: 'Oh, you will definitely see why.',
+    videoId: 1
+  },
+  {
     title: 'Cats being cute',
     postedBy: 'christian',
     description: 'Two brother cats cleaning themselves.',
-    videoId: 1
+    videoId: 2
   },
   {
     title: 'Dogs are the best',
     postedBy: 'hillary',
     description: 'Dog takes out the trash every day for his elderly owner.',
-    videoId: 2
+    videoId: 3
+  },
+  {
+    title: 'How to make peace with anyone',
+    postedBy: 'shubham',
+    description: 'A non-confrontational guide to confronting people who are just begging for it',
+    videoId: 4
   },
   {
     title: 'Chipmunks over the rainbow',
     postedBy: 'christine',
     description: 'a community of chipmunks show their LGBTQ+ pride',
-    videoId: 3
-  },
-  {
-    title: 'How to paint your nails the RIGHT way',
-    postedBy: 'shubham',
-    description: 'Do`s and Don`t`s for those new to nail care.',
-    videoId: 4
+    videoId: 5
   },
   {
     title: 'How wrestling with AWS nearly killed me',
     postedBy: 'jay',
     description: 'One man`s story of struggle, heartache, and ultimate redemption.',
-    videoId: 5
+    videoId: 6
   }
 ];
