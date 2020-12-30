23db748cc6def060daadcfb5d4a02c4b9b9388f7
christine <christamsnow@gmail.com>
2020-12-30 16:20:51 -0500

    routerlinkactive attempt to hide irrelevant menu links with css hidden visibility failed

--- src/app/sections-of-page/header-bar/header-bar.component.ts
+++ src/app/sections-of-page/header-bar/header-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header-bar',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderBarComponent implements OnInit {
 
-  constructor() { }
+  router: string;
+
+  constructor(private _router: Router) {
+    this.router = _router.url;
+  }
 
   ngOnInit(): void {
   }
