6b1204df584f8308cbae980ab028c73afaa129e3
christine <christamsnow@gmail.com>
2021-01-05 21:34:19 -0500

    MyVideos page working

--- src/app/my-videos/my-videos.component.ts
+++ src/app/my-videos/my-videos.component.ts
@@ -1,23 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentChecked, Component, OnInit} from '@angular/core';
 import {MockVideos} from '../mock-videos';
 import {Video} from '../video';
+import {VideosService} from '../service/videos.service';
+import {AUTHENTICATED_USER} from '../service/authentication.service';
 
 @Component({
   selector: 'app-my-videos',
   templateUrl: './my-videos.component.html',
   styleUrls: ['./my-videos.component.css']
 })
-export class MyVideosComponent implements OnInit {
-  videos = MockVideos;
-  // // @ts-ignore
-  // selectedVideo = Video;
+export class MyVideosComponent implements OnInit, AfterContentChecked {
+  // videos = MockVideos;
+  videos!: Video[];
+  selectedVideo!: Video;
+  myVideos!: Video[];
+  username!: string | null;
 
-  constructor() { }
+  constructor(private videosService: VideosService) { }
 
   ngOnInit(): void {
+    this.getVideos();
+    this.username = sessionStorage.getItem(AUTHENTICATED_USER);
   }
 
-  // public onSelect(video: Video): void {
-  //   this.selectedVideo = video;
-  // }
+  ngAfterContentChecked(): void {
+    this.getMyVideos();
+    if (this.selectedVideo == null) {
+      this.onSelect(this.getMyLastVideo());
+    }
+  }
+
+  getVideos(): void {
+    this.videosService.getVideos()
+      .subscribe((videos: Video[]) => this.videos = videos);
+  }
+
+  public getMyVideos(): void {
+    this.myVideos = this.videos.filter(video => video.userName === this.username);
+  }
+
+  public getMyLastVideo(): Video {
+    return this.myVideos[this.myVideos.length - 1];
+  }
+
+  public onSelect(video: Video): void {
+    this.selectedVideo = video;
+  }
 }
