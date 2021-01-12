8bb32a1fa3cb9d77584ae46b678f0efac5d64f7a
Christian Anderson <>
2021-01-11 20:24:26 -0500

    current user data shows in edit profile component

--- src/app/edit-profile-page/edit-profile-page.component.ts
+++ src/app/edit-profile-page/edit-profile-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthenticationService} from '../service/authentication.service';
 import {UserService} from '../service/user.service';
@@ -15,6 +15,7 @@ export class EditProfilePageComponent implements OnInit {
   lastName = '';
   email = '';
   password = '';
+  invalidUpdate = false;
 
   constructor(
     private router: Router,
@@ -23,18 +24,31 @@ export class EditProfilePageComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    if (typeof this.username === 'string') {
+      this.userService.executeJWTGetUserService(this.username)
+        .subscribe(
+          response => {
+            this.firstName = response.firstName;
+            this.lastName = response.lastName;
+            this.email = response.email;
+          }
+        );
+    }
   }
 
   handleJWTProfileUpdate = () => {
-    this.userService.executeJWTUpdateUserService(this.username, this.firstName, this.lastName, this.email,
-      this.password)
-      .subscribe(
-        response => {
-          console.log(response);
-        },
-        error => {
-          console.log(error);
-        }
-      );
+    if (typeof this.username === 'string') {
+      this.userService.executeJWTUpdateUserService(this.username, this.firstName, this.lastName, this.email,
+        this.password)
+        .subscribe(
+          response => {
+            console.log(`Response: ${response}`);
+          },
+          error => {
+            console.log(`Error: ${error}`);
+            this.invalidUpdate = true;
+          }
+        );
+    }
   }
 }
