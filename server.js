bf1622f52b79765082b599db60b4e14f64b0cee8
Jay <73120876+JarrellW@users.noreply.github.com>
2021-01-15 15:14:53 -0500

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<ziptube-demo>'));
+app.use(express.static(__dirname + '/dist/<ziptubefrontend>'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname + '/dist/<ziptube-demo>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/<ziptubefrontend>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
