5d279bf45e5c74f4ca0a87d1b5fb207df270198d
Jay <73120876+JarrellW@users.noreply.github.com>
2021-01-15 15:44:17 -0500

    heroku

--- src/app/server.js
+++ src/app/server.js
@@ -3,13 +3,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const cors = require('cors');
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<ziptubefrontend>'));
+app.use(cors());
+app.use(express.static('.dist/ziptube-demo'))
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname + '/dist/<ziptubefrontend>/index.html'));
+  res.sendFile(path.join(__dirname, './../dist/ziptubefrontend/index.html'));
+  console.log('path', path.join(__dirname, './../dist/ziptubefrontend/index.html'))
 });
 
 // Start the app by listening on the default Heroku port
